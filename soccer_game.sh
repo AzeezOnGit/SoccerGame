f5d2f838a51b5f651065dfc9e8ac0d5cd8614929
Azeez <azeez.oyelowo11@gmail.com>
2025-06-18 15:33:09 -0500

    new addition

--- soccer_game.sh
+++ soccer_game.sh
@@ -11,7 +11,7 @@ choose_class() {
 		read user_choice
 		echo "What a great selection. This is going to be great"
 		sleep 1
-		exit 0
+		return 0
 	else 
 		echo "Oh well, thanks for playing!"
 		return 1 #Exit early
@@ -25,7 +25,7 @@ choose_race() {
 		read race 
 		echo "Ha ha! I like that. Now we have your full player build."
 		sleep 1
-		exit 0
+		return 0
 	else
 		echo "Oh well, thanks for playing"
 		return 1 #Exit early
